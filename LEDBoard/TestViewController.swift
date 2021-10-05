7b75e125af69cf06cfb9e1c669b4a3f5a4289b81
Kobong <wangkobong@gmail.com>
2021-10-05 09:29:05 +0900

    second commit

--- LEDBoard/TestViewController.swift
+++ LEDBoard/TestViewController.swift
@@ -12,7 +12,8 @@ class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        print("")
     }
     
 
